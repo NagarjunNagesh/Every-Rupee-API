d3cef39814fe0ec4e15a94c17c70f3411fb23f03
Arjun <nagarjun_nagesh@outlook.com>
2020-01-01 20:27:20 +0100

    minor bugfix for user budget

--- src/test/java/in/co/everyrupee/controller/income/UserBudgetIntegrationTest.java
+++ src/test/java/in/co/everyrupee/controller/income/UserBudgetIntegrationTest.java
@@ -25,7 +25,6 @@ import java.util.Locale;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
