33807fee705ed4749dd26bf22c2c246b0da41d26
Arjun <nagarjun_nagesh@outlook.com>
2020-02-02 08:36:30 +0100

    corrected account trans relationship

--- src/main/java/in/co/everyrupee/service/income/UserTransactionService.java
+++ src/main/java/in/co/everyrupee/service/income/UserTransactionService.java
@@ -294,10 +294,9 @@ public class UserTransactionService implements IUserTransactionService {
 		// Auto update the bankaccount balance (Old Account)
 		eventPublisher.publishEvent(
 				new OnAffectBankAccountBalanceEvent(null, amountToModify, userTransaction.get().getAccountId()));
-		// If category is income then reverse (-)
-		if (categoryIncome) {
-			amountToModify *= -1;
-		}
+		// If category is income then reverse (-) or If category if expense then add (-)
+		amountToModify *= -1;
+		
 		// Auto update the bankaccount balance (New Account)
 		eventPublisher.publishEvent(new OnAffectBankAccountBalanceEvent(null, amountToModify, newAccountId));
 	}
